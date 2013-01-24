489fffe31c2b0fabc8d218d979197f7bb0c05637
Sami Niiranen <sami.i.niiranen@student.jyu.fi>
2013-01-24 23:54:46 +0200

    First version of database implemented with tests

--- seetweets/main/logic/SearchThread.py
+++ seetweets/main/logic/SearchThread.py
@@ -6,11 +6,13 @@ Created on Jan 22, 2013
 from threading import Thread
 import time
 from seetweets.main.logic.TwitterPoller import TwitterPoller
+from seetweets.main.Tweet import Tweet
 
 class SearchThread(Thread):
-    def __init__(self, tweetqueue):
+    def __init__(self, tweetqueue, database):
         Thread.__init__(self)
         self.tweetqueue = tweetqueue
+        self.database = database;
         
     def run(self):
         self.searchTweets()
@@ -19,10 +21,9 @@ class SearchThread(Thread):
         poller = TwitterPoller()
         
         while 1:
-            testjson = '{"results":[{"from_user_name":"Sami", "id":124125, "created_at":"20:57", "text":"Testaan etta toimiiko, 12345678 :-)"'
-                            
-            
-            json = poller.getTwitterSearchJson("suits")
-            tweet = poller.getLatestTweet(json)
+#            json = poller.getTwitterSearchJson("suits")
+#            tweet = poller.getLatestTweet(testjson)
+            tweet = Tweet(tid=1225215, hashtag="suits", time="20:57", author="@Sami", text="Testaan vaan etta toimiiko 1234556789 :)")
             self.tweetqueue.put(tweet)
+            tweet.persist(self.database.getConnection())
             time.sleep(10) 
---
+++ seetweets/tests/TestDatabase.py
@@ -0,0 +1,39 @@
+'''
+Created on Jan 24, 2013
+
+Unit tests for database
+
+@author: sailniir
+'''
+import unittest
+from seetweets.main.database.Database import Database
+from seetweets.main.Tweet import Tweet
+class TestDatabase(unittest.TestCase):
+    
+    def setUp(self):
+        self.database = Database("test")
+        
+        self.initDb()
+        
+    def initDb(self):
+        conn = self.database.getConnection();
+        
+        c = conn.cursor();
+        
+        c.execute('''DROP TABLE tweets''')
+        
+        c.execute('''CREATE TABLE IF NOT EXISTS tweets
+                    (tid INT, 
+                    hashtag TEXT, time TIMESTAMP, author TEXT, text TEXT)''')
+        
+        conn.commit()
+        conn.close()
+    
+    def testSave(self):
+        testTweet = Tweet(tid=12215, hashtag="test", time="20:57", author="@Sami", text="test case test case test case")
+        
+        self.assertEquals(0, self.database.getCount("tweets"), "The database should be empty!")
+        
+        testTweet.persist(self.database.getConnection())
+        
+        self.assertEquals(1, self.database.getCount("tweets"), "The database should have exactly one row!")
